3ffd6ad28ac4668be5bb3001aefa0b3e5e1b1e2e
basheerkattan <basheerkattan2060@gmail.com>
2022-11-08 16:39:35 +0100

    finished the flights page

--- react/src/routes/flights/FlightsForm.js
+++ react/src/routes/flights/FlightsForm.js
@@ -1,43 +1,59 @@
-import React, {Fragment} from "react";
-import classes from './FlightsForm.module.css'
+import React, { Fragment } from 'react';
+import classes from './FlightsForm.module.css';
 
-const FlightsForm = () =>{
-    const submitHandler = ()=>{
-        console.log('hi');
-    }
-    return(
-        <Fragment>
-            <form className= {classes.form} onSubmit={submitHandler}>
-                <div>
-                <label>From: </label>
-                <input type= 'text' placeholder= 'city'/>
-                </div>
-                <div> <label>To: </label>
-                <input type= 'text' placeholder= 'city'/>
-                </div>
-                <div><label>Departure: </label>
-                <input type='date' placeholder= 'Departure date'/>
-                </div>
-                <div><label>Return: </label>
-                <input type='date'  placeholder= 'Return'/>
-                </div>
-                <div> <label>Passengers: </label>
-                <input type='number' placeholder= 'Passengers'/>
-                </div>
-               
-                
-                
-               
-                
-            </form>
+const FlightsForm = () => {
+  const submitHandler = () => {
+    console.log('hi');
+  };
+  return (
+    <Fragment>
+      <div className={classes.main}>
+        <div className={classes.radio}>
+          <div>
+            <input type="radio" value="roundtrip" name="roundtrip" /> roundtrip 
+          </div>
+          <div>
+           <input type="radio" value="one way" name="one way" /> One way
+          </div>
+        </div>
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div>
+            <label>From: </label>
+            <input type="text" placeholder="City" />
+          </div>
+          <div>
+            {' '}
+            <label>To: </label>
+            <input type="text" placeholder="City" />
+          </div>
+          <div>
+            <label>Departure: </label>
+            <input type="date" placeholder="Departure date" />
+          </div>
+          <div>
+            <label>Return: </label>
+            <input type="date" placeholder="Return" />
+          </div>
+          <div>
+            {' '}
+            <label>Passengers: </label>
+            <input type="number" placeholder="Passengers" />
+          </div>
+        </form>
+        <div className={classes.checkBox}>
+          <div>
+            
+            <input type="checkbox" value="add" name="add" /> Add near by Airports
+          </div>
+          <div>
+         
+            <input type="checkbox" value="add" name="add" /> Non- stop flights only
+          </div>
+        </div>
+      </div>
+      <button className={classes.submitButton}>Search Flights</button>
+    </Fragment>
+  );
+};
 
-
-        </Fragment>
-    )
-
-
-}
-
-
-
-export default FlightsForm
+export default FlightsForm;
