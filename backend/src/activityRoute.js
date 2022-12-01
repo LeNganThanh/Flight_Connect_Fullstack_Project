da72d4fbd3769cf371a9fd5b997ddd5549d25436
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-12-01 09:39:49 +0100

    activities

--- backend/src/activityRoute.js
+++ backend/src/activityRoute.js
@@ -1,25 +1,25 @@
-import amadeus from './amadeus.js';
-import express from 'express';
+import amadeus from "./amadeus.js";
+import express from "express";
 
 const router = express.Router();
 
 const API = `api`;
 
-router.get(`/${API}/activities`, async(req, res) => {
-  const {latitude, longitude} = req.query
-  console.log(longitude)
+router.get(`/${API}/activities`, async (req, res) => {
+  const { latitude, longitude } = req.query;
 
-  try{
-      const response = await amadeus.shopping.activities.get({
-          latitude: latitude,
-          longitude: longitude,
-          radius: 20 
-      })
-      res.json(JSON.parse(response.body))
-  }catch(err){
-    console.log('oops')
-      res.json(err)
+  try {
+    const response = await amadeus.shopping.activities.get({
+      latitude: 41.397158,
+      longitude: 2.160873,
+      radius: 20,
+    });
+    console.log(response);
+    res.json(JSON.parse(response.body));
+  } catch (err) {
+    console.log("oops");
+    res.json(err);
   }
-})
+});
 
-export default router
+export default router;
