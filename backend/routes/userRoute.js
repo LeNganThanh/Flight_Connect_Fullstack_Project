733e485391880073d2c9908b6ae8b66d901db985
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-12-03 21:08:33 +0100

    add express.json() to index.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,33 +1,39 @@
-import express, { Router } from 'express';
-import { checkUserToken, getAllUsers, getUser, loginUser, createUser, updateUser, deleteUser } from '../controllers/userController.js';
-import { isAdmin } from '../middlewares/isAdminMiddleware.js';
-import verifyToken from '../middlewares/verifyToken.js';
-import { userValidation } from '../middlewares/validateMiddleware.js';
-const route = express.Router()
-
+import express, { Router } from "express";
+import {
+  checkUserToken,
+  getAllUsers,
+  getUser,
+  loginUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+import { isAdmin } from "../middlewares/isAdminMiddleware.js";
+import verifyToken from "../middlewares/verifyToken.js";
+import { userValidation } from "../middlewares/validateMiddleware.js";
+const route = express.Router();
 
 //===> Get All Users
 
-route.get('/', verifyToken, isAdmin, getAllUsers)
+route.get("/", verifyToken, isAdmin, getAllUsers);
 
 // ===> Login
-route.post('/login', loginUser)
+route.post("/login", loginUser);
 //===> verifyToken get
- route.get('/checkusertoken', checkUserToken)
+route.get("/checkusertoken", checkUserToken);
 
- //===> Get user 
- route.get('/:id', verifyToken, isAdmin, getUser)
+//===> Get user
+route.get("/:id", verifyToken, isAdmin, getUser);
 
- //===> create user
- route.post('/', userValidation, createUser)
+//===> create user
+route.post("/", userValidation, createUser);
 
- //===> update user
+//===> update user
 
- route.patch('/:id', verifyToken, isAdmin, updateUser)
+route.patch("/:id", verifyToken, updateUser);
 
- //===> Delete user
+//===> Delete user
 
-route.delete('/:id', verifyToken, isAdmin, deleteUser)
+route.delete("/:id", verifyToken, deleteUser);
 
 export default route;
-
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,70 +1,69 @@
 //===> Packages
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import multer from 'multer'
-import mongoose from 'mongoose';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import multer from "multer";
+import mongoose from "mongoose";
 //===> API routes
-import autocompRoute from './router.js';
-import searchRoute from './SearchRoute.js';
-import airportRoute from './airportRoute.js';
-import dealsRoute from './dealsRoute.js'
-import activityRoute from './activityRoute.js'
+import autocompRoute from "./router.js";
+import searchRoute from "./SearchRoute.js";
+import airportRoute from "./airportRoute.js";
+import dealsRoute from "./dealsRoute.js";
+import activityRoute from "./activityRoute.js";
 
 // ===> Backend local routes
-import userRoute from '../routes/userRoute.js'
-
+import userRoute from "../routes/userRoute.js";
 
 // ===> Mongoose Url
-import {MONGOOSE_URL} from './config.js'
-
+import { MONGOOSE_URL } from "./config.js";
 
 // ===> Setting the server
 const app = express();
 const PORT = 1338;
-  
+
 //===> Using the packages
-app.use(morgan('dev'));
-app.use(cors({ origin: '*' }));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 //===> Multer
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb){
-    let fullPath = './upload/';
-    cb(null, fullPath)
+  destination: function (req, file, cb) {
+    let fullPath = "./upload/";
+    cb(null, fullPath);
   },
-  fileName: function(req, file, cb){
-    let fileName = Date.now() + ' ' + file.originalName
-    cb(null, fileName)
-  }
-})
+  fileName: function (req, file, cb) {
+    let fileName = Date.now() + " " + file.originalName;
+    cb(null, fileName);
+  },
+});
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
-//===> Mongoose connection 
+//===> Mongoose connection
 
-mongoose.connect(MONGOOSE_URL)
+mongoose.connect(MONGOOSE_URL);
 
 //===> Routes
-app.use('/users',upload.single('image'), userRoute)
+app.use("/users", upload.single("image"), userRoute);
 
 //===> Applying handler for API
 
-app.use('/', autocompRoute);
-app.use('/', searchRoute);
-app.use('/', airportRoute);
-app.use('/', dealsRoute);
-app.use('/', activityRoute);
+app.use("/", autocompRoute);
+app.use("/", searchRoute);
+app.use("/", airportRoute);
+app.use("/", dealsRoute);
+app.use("/", activityRoute);
 
 //===> Static files
 
-app.use(express.static('../react/build'));
+app.use(express.static("../react/build"));
 
-app.get('/', (req, res) => {
-  res.sendFile('../react/build/index.html', { root: '.' });
+app.get("/", (req, res) => {
+  res.sendFile("../react/build/index.html", { root: "." });
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running on port:', PORT);
+  console.log("Server is running on port:", PORT);
 });
