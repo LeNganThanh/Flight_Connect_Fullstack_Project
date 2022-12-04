5823280847dd208d7848ad7594b06e6be789caaf
marin <mdabro85@gmail.com>
2022-12-04 17:27:23 +0100

    pushing the sidepanels for login and register

--- react/src/routes/flights/Flights.js
+++ react/src/routes/flights/Flights.js
@@ -6,8 +6,8 @@ import video from '../../media/video-2.mp4';
 const Flights = (props) => {
   return (
     <Fragment>
+    <Header />
       <div className={classes['main-display']}>
-        <Header />
         <header className={classes['header-txt']}>
           <h1 className={classes['header-txt-header']}>The best flight deals to <span>everywhere</span>, from <span>anywhere</span></h1>
           <FlightsForm search={props.search} setSearch={props.setSearch} dataSource={props.dataSource}  />
