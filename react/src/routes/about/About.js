cf71f3804f68fb3180b9a4a9640c6aac55cd526f
basheerkattan <basheerkattan2060@gmail.com>
2022-12-07 16:12:32 +0100

    Working on the about section

--- react/src/routes/about/About.js
+++ react/src/routes/about/About.js
@@ -1,37 +1,169 @@
-import React from "react";
-
+import React from 'react';
+import classes from './About.module.css';
+import logo from '../../media/logo-5.png';
 
 const About = () => {
-    return(
+  return (
+    <div>
+      <div className={classes.travel}>
         <div>
-            <p>
-                
-            What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-Where can I get some?
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-
-5
-	paragraphs
-	words
-	bytes
-	lists
-	Start with 'Lorem
-ipsum dolor sit amet...'
-†
-            </p>
+          <h2>Travel your way</h2>
         </div>
-    )
-}
+        <div className={classes.travelTxt}>
+          <p>
+            When we started in 2003, we set out to do things differently. We
+            wanted to show all flight options in one place, creating a simple
+            alternative to the confusing sites that make travel feel like hard
+            work.
+          </p>
+          <p>
+            Fast forward to today and we’/ve gone from a flight search engine to
+            a global leader in travel. We're proud that more than 100 million
+            people across the world (that's quite a few!) rely on our app and
+            website each month to help them with their travel plans.
+          </p>
+          <p>
+            Dedicated to making travel as simple as possible, we help each and
+            every one of them find the best options across flights, hotels, and
+            car rentals to book the perfect trip.
+          </p>
+        </div>
+      </div>
+      <div className={classes.logoBox}>
+        <div>
+          <h2>
+            It’s our mission to lead the global transformation to modern and
+            sustainable travel.
+          </h2>
+        </div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className={classes.modernTravel}>
+        <div>
+          <h2>What we mean by modern travel</h2>
+          <img
+            src="https://content.skyscnr.com/m/7e7bc1d67ce2a552/original/illustration-modern-travel.svg"
+            alt="woman on the way"
+          />
+        </div>
+
+        <div className={classes.modernTravelTxt}>
+          <p>
+            Travel is all about freedom. So it makes sense that planning and
+            booking your trip should be simple, not a chore.
+          </p>
+          <p>
+            We know you're looking for the best prices and most flexibility, to
+            choose what's right for you. Which is why we're always hard at work
+            to make sure our app and website are super straightforward and
+            efficient.
+          </p>
+          <p>
+            Choose where you want to go, when you want to go, and get the very
+            best price from thousands of sites, without having to look anywhere
+            else. Plus, check out all the ways we can help you find a trip
+            that's tailored to what you’re looking for, no matter your travel
+            style or your budget.
+          </p>
+          <p>
+            Feeling flexible? Search ‘Everywhere’ to see where you can go for a
+            great price. Got a destination in mind? Use our Price Alerts to find
+            out when prices change.
+          </p>
+          <p>
+            And once you know when and where you’re going, book in just a few
+            quick steps, whether on our app or website -- both of which are
+            available in more than 30 languages.
+          </p>
+        </div>
+      </div>
+      <div className={classes.modernTravel}>
+        <div>
+          <h2>What we mean by sustainable travel</h2>
+          <img
+            src="https://content.skyscnr.com/m/1330f2c70f1ea3a6/original/illustration-sustainable-travel.svg"
+            alt="woman on the way"
+          />
+        </div>
+
+        <div className={classes.modernTravelTxt}>
+          <p>
+            Travel is one of life's greatest pleasures. And we want each
+            generation to be able to experience that joy.
+          </p>
+          <p>
+            We give you all the options so that you can make the decision that’s
+            right for you. That includes our 'Greener choice' label, which
+            highlights the flights that emit less CO2 and we’ve already helped
+            more than 10 million travelers select flights with the lowest
+            emissions for their route.
+          </p>
+          <p>
+            It’s just the start, but we’re serious about sustainability and
+            holding ourselves accountable. We buy sustainable aviation fuel
+            (SAF) to help offset any travel we do here at Skyscanner. We're a
+            team that's committed to discovering new ways to explore the world
+            in a way that benefits both people and places.
+          </p>
+          <p>
+            We also know we can’t solve this challenge alone. That's why we’re
+            working with the Duke of Sussex and industry leaders, to form
+            Travalyst. Together, we’re a catalyst for change, committed to
+            helping everyone explore our world for generations to come.
+          </p>
+        </div>
+      </div>
+      <div className={classes.dealsMain}>
+        <div className={classes.dealsTxt}>
+          <h2>Looking for the best flight deals to anywhere in the world?</h2>
+          <p>
+            It’s easy around here. 100 million travelers use us as their go-to
+            tool, comparing flight deals and offers from more than 1,200
+            airlines and travel providers. With so many options to choose from
+            in one place, you can say hello to savings and goodbye to stress –
+            here’s how.
+          </p>
+        </div>
+        <div className={classes.dealsBox}>
+          <div>
+            <img
+              src="https://content.skyscnr.com/m/3e55723dcc8a2d0f/original/image1.svg"
+              alt="world"
+            />
+            <h3>Search Everywhere explore anywhere</h3>
+            <p>
+              Enter your departure airport and travel dates, then hit
+              Everywhere. You'/ll see flights to every destination in the world,
+              cheapest first.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://content.skyscnr.com/m/7b7b3eaf2ab4081c/original/image2.svg"
+              alt="offer"
+            />
+            <h3>Pay less, go further with transparent pricing</h3>
+            <p>
+              The cheapest flight deals. No hidden fees. No funny business. With
+              us, the price you see when you search is what you’/ll pay.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://content.skyscnr.com/m/4ab58b7e3ae3790c/original/image3.svg"
+              alt="clock"
+            />
+            <h3>Book when it's best with Price Alerts</h3>
+            <p>
+              Found your flight, but not quite ready to book? Set up Price
+              Alerts and we’ll let you know when your flight price goes up or
+              down.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- react/src/routes/offers/Activities.js
+++ react/src/routes/offers/Activities.js
@@ -1,33 +1,39 @@
-import React, { useState, useContext, useLayoutEffect, } from "react";
-import classes from "./Activities.module.css";
-import { FlightsContext } from "../../context/FlightsContext";
+import React, { useState, useContext, useLayoutEffect } from 'react';
+import classes from './Activities.module.css';
+import { FlightsContext } from '../../context/FlightsContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronRight,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import Button from '../../components/Button.js'
 
 const Activities = props => {
   const [state] = useContext(FlightsContext);
   const { activities } = state;
 
-  const inputTo = document.getElementById("to");
+  const inputTo = document.getElementById('to');
 
   //function use to clear all html tags includes in description from api
   function extractContent(html) {
-    return new DOMParser().parseFromString(html, "text/html").documentElement
+    return new DOMParser().parseFromString(html, 'text/html').documentElement
       .textContent;
   }
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   //const [photoCounter, setPhotoCounter] = useState(0)
 
   const previous = () => {
     if (counter > 0) {
-      setCounter(counter - 1)
+      setCounter(counter - 1);
       //setPhotoCounter(0)
     }
-  }
+  };
   const next = () => {
-    if (counter < (activities[0].length - 1)) {
-      setCounter(counter + 1)
+    if (counter < activities[0].length - 1) {
+      setCounter(counter + 1);
       //setPhotoCounter(0)
     }
-  }
+  };
 
   // const prevPhoto = () => {
   //   if (photoCounter > 0) {
@@ -41,34 +47,43 @@ const Activities = props => {
   // }
 
   const imageRef = () => {
-    const search = activities[0][counter].name
-    const result =`https://www.google.com/maps/search/${search}`
-    return result
-  }
+    const search = activities[0][counter].name;
+    const result = `https://www.google.com/maps/search/${search}`;
+    return result;
+  };
 
   useLayoutEffect(() => {
-    setCounter(0)
+    setCounter(0);
     //setPhotoCounter(0)
-  }, [activities])
-  
+  }, [activities]);
+
   return (
     <div>
       {activities.length > 0 ? (
         <div>
           <h2 className={classes.topTitle}>Attractions in {inputTo.value}</h2>
           <div className={classes.activBox}>
-            <button onClick={previous}>{'<'}</button>
-                <div>
-                  <div className={classes.article}>
-                    <a href={imageRef()} target='_blank'><img src={activities[1][counter]}></img></a>
-                    <h3>{activities[0][counter].name} </h3>
-                    <div className={classes.para}>
-                      <p>{extractContent(activities[0][counter].types.join(' | '))}</p>
-                    </div>
-                  </div>
+              <Button className={classes.slidesBtn} onClick={previous}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </Button>
+            <div>
+              <div className={classes.article}>
+                <a href={imageRef()} target="_blank">
+                  <img src={activities[1][counter]} alt='slide'></img>
+                </a>
+                <h3>{activities[0][counter].name} </h3>
+                <div className={classes.para}>
+                  <p>
+                    {extractContent(activities[0][counter].types.join(' | '))}
+                  </p>
                 </div>
-            <button onClick={next}>{'>'}</button>
-         </div>
+                  
+              </div>
+            </div>
+                <Button className={classes.slidesBtn} onClick={next}>
+                <FontAwesomeIcon icon={faChevronRight} />
+                </Button>
+          </div>
         </div>
       ) : null}
     </div>
