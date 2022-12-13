fb1f1a292f46e21a4fce1ae82808cf8ec2efd3ac
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-12-13 17:01:48 +0100

    commit after merging

--- react/src/components/Login.js
+++ react/src/components/Login.js
@@ -1,37 +1,44 @@
-import Button from './Button.js';
-import toast, { Toaster } from 'react-hot-toast';
-import {FlightsContext} from '../context/FlightsContext'
-import { useContext } from 'react';
+import Button from "./Button.js";
+import toast, { Toaster } from "react-hot-toast";
+import { FlightsContext } from "../context/FlightsContext";
+import { useContext } from "react";
 
 export default function Login(props) {
-    const [state, dispatch] = useContext(FlightsContext)
+  const [state, dispatch] = useContext(FlightsContext);
 
   const loginUser = e => {
     e.preventDefault();
 
-   const data = JSON.stringify({email: e.target.email.value, password: e.target.password.value})
+    const data = JSON.stringify({
+      email: e.target.email.value,
+      password: e.target.password.value,
+    });
     console.log(data);
-    fetch('http://localhost:1338/users/login', { method: 'POST', headers: {'Content-Type': 'application/json'},body: data })
+    fetch("http://localhost:1338/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    })
       .then(res => {
-        const token = res.headers.get('token')
-        localStorage.setItem('token', token)
-        return res.json()
+        const token = res.headers.get("token");
+        localStorage.setItem("token", token);
+        return res.json();
       })
       .then(result => {
         console.log(result);
         if (result.success) {
-          toast.success('Successfully Logged In!');
+          toast.success("Successfully Logged In!");
           dispatch({
-            type: 'setUser',
-            user: result.data
-          })
+            type: "setUser",
+            user: result.data,
+          });
           setTimeout(() => {
             props.setRegister(false);
-           dispatch({
-            type: 'setLogin',
-            login: false
-           })
-            props.setLoggedIn(true)
+            dispatch({
+              type: "setLogin",
+              login: false,
+            });
+            props.setLoggedIn(true);
           }, 2000);
         } else {
           toast.error(JSON.stringify(result.message));
@@ -42,7 +49,6 @@ export default function Login(props) {
     <div className={props.className}>
       <form onSubmit={loginUser}>
         <div>
-          <label>Email:</label>
           <input type="email" id="email" name="email" required></input>
         </div>
         <div>
